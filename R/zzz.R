c74ef9658ae88d5adab9713fc27cfb571a88dd54
Roger D. Peng <rpeng@jhsph.edu>
2003-11-03 00:00:00 +0000

    version 1.0-1

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
-.First.lib <- function(lib, pkg) {
+.onLoad <- function(lib, pkg) {
     where <- match(paste("package:", pkg, sep = ""), search())
     ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
     ver <- as.character(ver)
     cat(paste("Simple Bootstrap Routines (version ", ver, ")\n", sep = ""))
-
-    library(boot)
-    library(modreg)
+    if(!require(boot))
+        stop(sQuote("boot"), " library required for ", sQuote("simpleboot"))
+    library(stats)
 }
