2f5e79a48dab389dd4f76b60cf3942a513edd9d9
Roger D. Peng <rpeng@jhsph.edu>
2019-02-20 21:00:03 +0000

    version 1.1-7

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,7 @@
 .onAttach <- function(lib, pkg) {
     dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
-    msg <- gettextf("%s (%s %s)", dcf[, "Title"],
-                    as.character(dcf[, "Version"]), dcf[, "Date"])
+    msg <- gettextf("%s (%s)", dcf[, "Title"],
+                    as.character(dcf[, "Version"]))
     packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
-}
-
-.onLoad <- function(lib, pkg) {
-    if(!require(boot))
-        stop("'boot' package required")
 }
 
